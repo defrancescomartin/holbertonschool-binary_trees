2a9165652f660cc2bce8a6228de809a3c177d63d
defrancescomartin <defrancescoma@gmail.com>
2022-07-18 17:18:56 -0300

    task 13 node counter

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -17,7 +17,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		nodes++;
 	nodes = nodes + binary_tree_nodes(tree->left);
 	nodes = nodes + binary_tree_nodes(tree->right);
-	nodes = nodes + 1;
 
 	return (nodes);
 }

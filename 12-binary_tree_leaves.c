cb57ae237ae41f215f68cb76dcf72513593a2a30
defrancescomartin <defrancescoma@gmail.com>
2022-07-18 17:05:27 -0300

    task 12 leaves counter

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_leaves - counts the leaves in a binary tree
- * @tree: pointer to the root node of the tree to measure the size
+ * @tree: pointer to the root node of the tree to count the number of leaves
  * Return: leaves
  **/
 

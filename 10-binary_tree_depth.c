3c5e13d9c79fab0504748311f05079484b6793ec
defrancescomartin <defrancescoma@gmail.com>
2022-07-18 16:08:12 -0300

    task 10 depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -15,8 +15,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 
 	if (tree->parent != NULL)
 	{
-		depth = binary_tree_depth(tree->parent);
-		depth++;
+		depth = binary_tree_depth(tree->parent) + 1;
 	}
 	
 	return (depth);
